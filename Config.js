2f537fbd6834d2b9106fa3bddc216c2ef6ff867e
toji002 <168723133+toji002@users.noreply.github.com>
2024-05-02 02:28:24 -1200

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,16 +3,16 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "+994407728657"],
+global.ownername = process.env.OWNER_NAME || "TOᒍI__ᖴᑌSᕼIGᑌᖇO",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "Trafalgar D. Water Law",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "",
 global.packname = 'Sticker By'
 global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
 //console view/theme
